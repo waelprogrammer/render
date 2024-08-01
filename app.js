269b4e624fc27351c3b251d0fc105f9ffc038731
Wael <waelprogrammer028@gmail.com>
2024-08-01 19:55:58 +0300

    adding new endpoint

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 3030; // 2eza 2deret l2et file .env fiya kelmet port sta3mel al value taba3a wa 2ela sta3mel 303  // fa hala2 ne7na metel ka2anno sma7na la render.com y7et howeh men 3endo file .env w y7et l port yalli bado yeh
 // console.log(process.env.wael);
   app.get('/', function (req, res) {
-   res.send('Hello World')   
+   res.send('Ali 2enta 5ara ')   
 })
-
+app.get('/students', function (req, res) {
+    let data = [
+        {id:1,name:'Mohammad'},
+        {id:2,name:'Wael'},
+        {id:3,name:'ALi'},
+        {id:4,name:'Brahim'}
+    ]
+    res.send(data); 
+ })
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
