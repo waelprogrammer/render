a35b50cfdb876c48730197b3d06f83cc4dc3833c
Wael <waelprogrammer028@gmail.com>
2024-08-03 13:55:36 +0300

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+let cors = require('cors');
+
 const app = express();
 const xlsx = require('xlsx');
 let wb = xlsx.readFile('excel.xlsx');
 let ws = wb.Sheets['names'];
 let data = xlsx.utils.sheet_to_json(ws);
  // require('dotenv').config();
-
+app.use(cors());
 const PORT = process.env.PORT || 3030; // 2eza 2deret l2et file .env fiya kelmet port sta3mel al value taba3a wa 2ela sta3mel 303  // fa hala2 ne7na metel ka2anno sma7na la render.com y7et howeh men 3endo file .env w y7et l port yalli bado yeh
 // console.log(process.env.wael);
   app.get('/', function (req, res) {
